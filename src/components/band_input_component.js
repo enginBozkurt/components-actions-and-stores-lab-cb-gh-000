1be13e203a1af6f5013bf7ca7bf0d510ad1602a0
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-08-04 00:31:25 +0000

    Components Actions And Stores Lab

--- src/components/band_input_component.js
+++ src/components/band_input_component.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react'
-class BandInputComponent extends Component {
-  render(
+
+export default class BandInput extends Component {
+
+  handleBandShow(){
+   return this.props.store.getState().map((band, i) => <BandIndex key={i} band={band} />)
+ }
+
+  onSubmit(event){
+      event.preventDefault();
+      this.props.store.dispatch({type: 'ADD_BAND', payload: {title: event.target.children[1].value}})
+      event.target.children[1].value = ''
+  }
+
+  render(){
     return(
-      <div>hello</div>
-    )
-  )
+      <div>
+        <form onsubmit={this.onSubmit.bind(this)}>
+            <label>Enter Band: </label>
+            <input />
+            <button type="submit">Submit</button>
+        </form>
+        <h1>Bands</h1>
+        <ul>
+          {this.handleBandShow()}
+        </ul>
+      </div>
+    );
+  }
 }
